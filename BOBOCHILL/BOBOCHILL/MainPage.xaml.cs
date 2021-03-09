dbc850ab20a2f76ddd06b21ea06275d1e4b42b47
FrancescoGlasnovic <64422825+FrancescoGlasnovic@users.noreply.github.com>
2021-03-09 22:41:50 +0100

    Ploca&IzlazVan
    Ploca je postavljena, i igraci kad dobe 6 mogu izvuc figuricu van

--- BOBOCHILL/BOBOCHILL/MainPage.xaml.cs
+++ BOBOCHILL/BOBOCHILL/MainPage.xaml.cs
@@ -26,5 +26,88 @@ namespace BOBOCHILL
         {
             this.InitializeComponent();
         }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBlock_SelectionChanged_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBlock_SelectionChanged_2(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        int naRedu = 1;
+        int brBacanja = 0;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            
+            Random rnd = new Random();
+            int d;
+            d = rnd.Next(1, 7);
+            Kocka.Text = Convert.ToString(d);
+            if (d == 6)
+            {
+                if (naRedu == 1)
+                {
+                    B_Izlaz_1_.Text = B1.Text;
+                }
+                else if (naRedu == 2)
+                {
+                    Y_Izlaz_11_.Text = Y1.Text;
+                }
+                else if (naRedu == 3)
+                {
+                    G_Izlaz_21_.Text = G1.Text;
+                }
+                else if (naRedu == 4)
+                {
+                    R_Izlaz_31_.Text = R1.Text;
+                }
+            }
+            brBacanja++;
+            BrBac.Text = Convert.ToString(brBacanja);
+                if(brBacanja==3)
+                    {
+                naRedu++;
+                brBacanja = 0;
+                    }
+            
+            if(naRedu==5)
+            {
+                naRedu = 1;
+            }
+        }
+
+        private void Kocka_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void TextBlock_SelectionChanged_B1(object sender, RoutedEventArgs e)
+        {
+            B1.Text = "B1";
+            
+        }
+
+        private void TextBlock_SelectionChanged_IzlazB(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BrBac_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
